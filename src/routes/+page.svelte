941b2e06e41b79611b3d63451fcfaa8578c46481
ChrstphrHll <thchrstphrhll@gmail.com>
2026-08-25 17:27:04 -0400

    init commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,37 @@
+<div class="container">
+  <h1>ChrstphrHll</h1>
+  <p>That's me! I make cool stuff and put it on the internet.</p>
+
+  <ul>
+    <li>
+      <a href="https://swu-draft.pages.dev" target="_parent">Starwars Unlimited Multiplayer Draft Simulator</a>
+    </li>
+    <li>
+      <a href="https://pitazines.pages.dev/">Pita Zines</a> a gift I made for my partner
+    </li>
+    <li>
+      <a href="https://chrstphrhll.itch.io/buylowselleyes">Buy Low; Sell Eyes</a> a four day game jam game
+    </li>
+    <li>
+      <a href="https://chrstphrhll.github.io/DukeBotWBingo/">Duke Breath of the Wild Bingo</a>
+    </li>
+    <li>
+      <a href="https://democratichelphub.org/">Durham Help Hub</a> an info website for a mutual aid initiative in Durham
+    </li>
+    <li>
+      <a href="https://chrstphrhll.pages.dev/">ChrstphrHll</a> a portfolio websites with no secrets or puzzles; its just normal
+    </li>
+    <li>
+      <a href="https://compcon.app/">COMP/CON</a> a <i>Lancer</i> RGP companion app that I've made community contributions to
+    </li>
+  </ul>
+</div>
+
+
+<style>
+  .container {
+    max-width: 800px;
+    margin-inline: auto;
+    margin-top: 64px;
+  }
+</style>
