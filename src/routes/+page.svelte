e895549430e5196f288469c70f140e3a5d991748
ChrstphrHll <thchrstphrhll@gmail.com>
2026-08-26 10:28:34 -0400

    further video optimiaztion

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -32,7 +32,7 @@
 <hr>
 <p>And here's a micro controller I taught to play <i>Celeste</i> really really fast with a friend</p>
 <video controls>
-  <source src="/celeste_tas.webm">
+  <source src="/celeste_tas.webm" type="video/webm">
 </video>
 
 <style>
