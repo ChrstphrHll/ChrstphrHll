5a551b4e653ca52969ea0ea6b199c7690e44635d
ChrstphrHll <thchrstphrhll@gmail.com>
2026-08-26 12:44:35 -0400

    cover letter mix up

---
+++ src/routes/coverletter/+page.svelte
@@ -0,0 +1,3 @@
+<h1>Outdated Cover Letter</h1>
+
+<p>Turns out I had an issue updating my cover letter in the application system. View the version that should have been submitted <a href="coverletter.pdf">here</a>. You can also check out my other projects <a href="/">here</a></p>
