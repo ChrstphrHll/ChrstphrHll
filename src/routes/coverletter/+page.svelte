ba134b60987a3c9f430c09167f97b4b2a31d9ce8
ChrstphrHll <thchrstphrhll@gmail.com>
2026-08-25 21:49:53 -0400

    cover letter place holder

---
+++ src/routes/coverletter/+page.svelte
@@ -0,0 +1,3 @@
+<h1>Video Cover Letter</h1>
+
+<p>Oops, looks like the video cover letter isn't quite done yet. I was quite eager to get my application in, so I submitted all my written materials. Check back in a day or two and my cinematic masterpiece will be uploaded and convincing you that I'm the one for the job. While you're here tho, might as well take a look at <a href="/">some of my other projects</a>.</p>
